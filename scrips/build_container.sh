e6945a1ae10964f8937c99ea3f12976d1b5713ea
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2017-12-04 21:44:41 +0200

    debug buil.sh

--- scrips/build_container.sh
+++ scrips/build_container.sh
@@ -4,7 +4,5 @@ sudo docker build -t lua_nginx docker_folder > raw.log
 
 IMG_ID=$(cat raw.log | grep "Successfully built" | awk '{print $3}')
 
-
 sudo docker tag $IMG_ID boodman/lua_nginx
 sudo docker push boodman/lua_nginx
-sudo docker rmi -f $IMG_ID
