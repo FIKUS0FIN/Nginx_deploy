4617291a7819d7e66926755725e2a9b751006280
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2017-12-05 19:36:33 +0200

    new Jenkinsfile and test_data upload

--- scrips/run_container.sh
+++ scrips/run_container.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-sudo docker run -d -p 80:80 boodman/lua_nginx
+p_m_n=$(cat ../aws_d-m.conf)
+sudo docker-machine ssh $p_m_n sudo docker stop lua_nginx
+exit 0
+sudo docker-machine ssh $p_m_n sudo docker run -d -p 80:80 --name lua_nginx boodman/lua_nginx
