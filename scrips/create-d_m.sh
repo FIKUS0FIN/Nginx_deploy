0dd9b5e3e6f19cf2ddb7af7f537d9bedb6930825
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2017-12-05 14:24:59 +0200

    delliting sudo from script

--- scrips/create-d_m.sh
+++ scrips/create-d_m.sh
@@ -10,8 +10,8 @@ function kill_aws()
   if [[ [$1 -eq 0] || [$2 -eq 0]  ]]; then
     previous_mashine=$1
     previous_mashine_name=$2
-    sudo docker-machine kill $previous_mashine
-    sudo docker-machine rm -y $previous_mashine_name
+    docker-machine kill $previous_mashine
+    docker-machine rm -y $previous_mashine_name
   fi
 }
 
@@ -19,13 +19,13 @@ function create_ec2()
 {
   NAME=$1
   #=============================================================================
-  sudo docker-machine create --driver amazonec2 --amazonec2-open-port ${PORT} \
+  docker-machine create --driver amazonec2 --amazonec2-open-port ${PORT} \
   --amazonec2-region ${REGION} ${NAME}-${BUILD_ID}
 }
 
 function put_info
 {
-  AWS_ID=$(sudo docker-machine inspect ${NAME}-${BUILD_ID} | grep InstanceId \
+  AWS_ID=$(docker-machine inspect ${NAME}-${BUILD_ID} | grep InstanceId \
   | awk '{print $2}' | awk -F "\"" '{print $2}')
   echo "${NAME}-${BUILD_ID}" > ../aws_d-m.conf
   awless show $AWS_ID
@@ -35,7 +35,7 @@ function put_info
 function deploy_container()
 {
   container=$1
-  sudo docker-machine ssh ${NAME}-${BUILD_ID} sudo docker run -d -p ${PORT}:80 $container
+  docker-machine ssh ${NAME}-${BUILD_ID} sudo docker run -d -p ${PORT}:80 $container
   #statements
 }
 
